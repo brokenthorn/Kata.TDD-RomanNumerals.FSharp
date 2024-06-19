a38dfbd34db9b9d45fd57275aeacf7bfa38c8720
Paul-Sebastian Manole <paul.manole@thoughtworks.com>
2024-06-20 02:50:14 +0300

    feat: Add conversion for 1

--- src/App.Lib/Library.fs
+++ src/App.Lib/Library.fs
@@ -1,4 +1,7 @@
 ﻿module App.Lib
 
 let toRomanNumeral (num: uint32): string =
-    ""
+    match num with
+    | 0u -> ""
+    | 1u -> "I"
+    | _ -> ""
--- src/App.Tests/Tests.fs
+++ src/App.Tests/Tests.fs
@@ -8,3 +8,9 @@ let ``Romans didn't have a 0`` () =
     let zero = 0u
     let roman = toRomanNumeral zero
     Assert.Equal("", roman)
+
+[<Fact>]
+let ``Converts 1 to I`` () =
+    let one = 1u
+    let roman = toRomanNumeral one
+    Assert.Equal("I", roman) 
