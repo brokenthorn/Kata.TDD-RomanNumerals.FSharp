68bcddf0f87e12a65823cc741392797ff036bfcc
Paul-Sebastian Manole <paul.manole@thoughtworks.com>
2024-06-24 13:19:45 +0300

    refactor: Generalize implementation

--- src/App.Lib/Library.fs
+++ src/App.Lib/Library.fs
@@ -1,9 +1,19 @@
 ﻿module App.Lib
 
-/// Takes an unsigned 32bit integer and converts it to a Roman numeral string.
-let toRomanNumeral (num: uint32) : string =
-    match num with
+let private standardArabicToRomanPairs = [|
+    (5u, "V")
+    (1u, "I")
+|]
+
+let private getFactor (arabicNumber: uint32) =
+    Array.find
+    <| fun pair -> (pair |> fst) <= arabicNumber
+    <| standardArabicToRomanPairs
+
+/// Takes an unsigned Arabic number and converts it to a Roman numeral.
+let rec toRomanNumeral (arabicNumber: uint32) : string =
+    match arabicNumber with
     | 0u -> ""
-    | 1u -> "I"
-    | 5u -> "V"
-    | _ -> failwith (sprintf "Could not convert %u to a Roman numeral" num)
+    | _ ->
+        let (arabicPair, romanPair) = getFactor arabicNumber
+        romanPair + toRomanNumeral (arabicNumber - arabicPair)
--- src/App.Tests/Tests.fs
+++ src/App.Tests/Tests.fs
@@ -3,10 +3,11 @@ module Tests
 open Xunit
 open App.Lib
 
-let testData: obj[] list = [ [| 0; "" |]; [| 1; "I" |]; [| 5; "V" |] ]
+/// Test data that maps an Arabic number to the correct Roman numeral.
+let testData: obj[] list = [ [| 0; "" |]; [| 1; "I" |]; [| 2; "II" |]; [| 5; "V" |] ]
 
 [<Theory>]
 [<MemberData(nameof (testData))>]
-let ``Converts Arabic number to Roman numeral`` number expected =
-    let actual = toRomanNumeral number
-    Assert.Equal(expected, actual)
+let ``Converts Arabic number to Roman numeral`` arabicNumber expectedRomanNumeral =
+    let romanNumeral = toRomanNumeral arabicNumber
+    Assert.Equal(expectedRomanNumeral, romanNumeral)
